792e68100b2fb51fd751adbd98ca6e45e66b44f3
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-06-14 13:51:09 +0330

    Disable Local Users.ps1

--- User Management/Disable Local Users.ps1
+++ User Management/Disable Local Users.ps1
@@ -1,1 +1,6 @@
+#abdi
 get-localuser | ? {($_.name -ne 'Administrator')} | disable-localuser -Confirm:$false
+
+
+#https://learn.microsoft.com/en-us/answers/questions/1040617/remove-disable-local-accounts-except-administrator
+Get-LocalUser | Where-Object {$_.Name -ne "Administrator"} | Disable-LocalUser -WhatIf
